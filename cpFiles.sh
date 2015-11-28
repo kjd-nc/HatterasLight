988d951becc546264f9e146894db8fbbc783811c
Kevin Dunphy <kjdunphy@bellsouth.net>
2015-11-28 12:49:13 -0500

    Make the lighthouse flash
     Changes to be committed:
    	modified:   HatterasLight.ac
    	modified:   HatterasLight.skp
    	new file:   HatterasLight.xml
    Thumbnail for the objects database
    	new file:   HatterasThumbnail.jpg
    Helper to update files
    	new file:   cpFiles.sh

---
+++ cpFiles.sh
@@ -0,0 +1,6 @@
+#~/bin/sh -xv
+
+TARGET="~/Library/Application\ Support/FlightGear/TerraSync/Models/Maritime/Misc"
+
+ln -s ./HatterasLight.ac $TARGET/HatterasLight.ac
+ln -s ./HatterasLight.xml $TARGET/HatterasLight.xml
